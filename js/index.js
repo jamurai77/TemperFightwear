27d8ab4d629de11aef72110a605fa3bf83380b94
Jamurai77 <jamurai77@yahoo.com>
2025-05-09 10:22:24 -0400

    carousel next button finished

--- js/index.js
+++ js/index.js
@@ -6,24 +6,21 @@ const searchIcon = document.querySelector('.bx-search');
 const searchBar = document.querySelector('.search-bar');
 const menuTitle = document.querySelector('.menu-title');
 
-const slider = document.querySelector('.hero__carousel__slides');
+const slider = document.querySelector('.hero__carousel__slider');
 const slides = Array.from(slider.children);
 const prevButton = document.querySelector('.hero__carousel__button--left');
 const nextButton = document.querySelector('.hero__carousel__button--right');
 const dotsNav = document.querySelector('.hero__carousel__indicators');
 const dots = Array.from(dotsNav.children);
-const slideWidth = slides[0].getBoundingClientRect().width;
 
-const setSlidePosition = (slide, index) => {
-    slide.style.left = slideWidth * index + 'px';
-}
 
 const moveToSlide = (slider, currentSlide, targetSlide) => {
-    slider.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+    const targetIndex = slides.indexOf(targetSlide);
+    const amountToMove = targetIndex * 100;
+    slider.style.left = '-' + amountToMove + '%';
     currentSlide.classList.remove('current-slide');
     targetSlide.classList.add('current-slide');
 }
-// console.log(slideWidth);
 
 hamburger.addEventListener('click', () => {
     sidebar.classList.toggle('sidebar--visible');
@@ -38,25 +35,30 @@ searchIcon.addEventListener('click', () => {
     searchBar.classList.toggle('search-open');
 })
 
-slides.forEach(setSlidePosition);
 
-prevButton.addEventListener('click', e => {
-    const currentSlide = slider.querySelector('.current-slide');
-    const prevSlide = currentSlide.previousElementSibling;
-    moveToSlide(slider, currentSlide, prevSlide);
-})
+// prevButton.addEventListener('click', e => {
+//     const currentSlide = slider.querySelector('.current-slide');
+//     const prevSlide = currentSlide.previousElementSibling;
+//     moveToSlide(slider, currentSlide, prevSlide);
+// })
 
 nextButton.addEventListener('click', e => {
     const currentSlide = slider.querySelector('.current-slide');
-    const nextSlide = currentSlide.nextElementSibling;
-    moveToSlide(slider, currentSlide, nextSlide);
+    const targetSlide = currentSlide.nextElementSibling;
+    moveToSlide(slider, currentSlide, targetSlide);
 })
 
-dotsNav.addEventListener('click', e => {
-    const targetDot = e.target.closest('button');
+prevButton.addEventListener('click', e => {
     const currentSlide = slider.querySelector('.current-slide');
-    const currentDot = dotsNav.querySelector('.current-slide');
-    if (!targetDot) {
-
-    }
+    const targetSlide = currentSlide.previousElementSibling;
+    moveToSlide(slider, currentSlide, targetSlide);
 })
+
+// dotsNav.addEventListener('click', e => {
+//     const targetDot = e.target.closest('button');
+//     const currentSlide = slider.querySelector('.current-slide');
+//     const currentDot = dotsNav.querySelector('.current-slide');
+//     if (!targetDot) {
+
+//     }
+// })
